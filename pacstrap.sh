c5769431d6cd197c1786e1bc134ab4bf0fc640ca
XADE <xade@noreply.codeberg.org>
2024-02-29 13:58:55 +0000

    dependency added: libnghttp3;

--- pacstrap.sh
+++ pacstrap.sh
@@ -5,32 +5,48 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 
 # usage: sh pacstrap.sh <directory>
-# requirements: sh curl gawk tar gzip xz zstd 
+# requirements: sh curl gawk tar gzip xz zstd
 
 # check requirements
 type sh curl awk tar gzip xz zstd || exit 1
 mkdir -p "$1/var/cache/pacman/pkg"
 cd "$1" || exit 1
 DIR="$(pwd)"
+TMP="$DIR/tmp"
 # don't redownload the packages
 cd "var/cache/pacman/pkg" || exit 1
 ARCH="(i686|x86_64|any)"
-URL="https://mirrors.tuna.tsinghua.edu.cn/artixlinux/system/os/x86_64"
-curl -L "$URL" > "repo_content" || exit 1
-PACKAGES="acl artix-mirrorlist bash brotli bzip2 ca-certificates ca-certificates-mozilla ca-certificates-utils coreutils curl e2fsprogs expat filesystem findutils glibc gpgme keyutils krb5 libarchive libassuan libffi libgpg-error libidn2 libnghttp2 libp11-kit libpsl libssh2 libtasn1 libunistring lz4 ncurses openssl p11-kit pacman readline xz zlib zstd"
+URL=${2:-https://mirrors.tuna.tsinghua.edu.cn/artixlinux/system/os/x86_64}
+curl -L "$URL" >"repo_content" || exit 1
+PACKAGES="[a-z].*-mirrorlist acl bash brotli bzip2 ca-certificates ca-certificates-mozilla ca-certificates-utils coreutils curl e2fsprogs expat filesystem findutils glibc gpgme keyutils krb5 libarchive libassuan libffi libgpg-error libidn2 libnghttp2 libnghttp3 libp11-kit libpsl libssh2 libtasn1 libunistring lz4 ncurses openssl p11-kit pacman readline xz zlib zstd"
+# unpack
+EXTRACT() {
+  for COMPRESSION in --zstd --gzip --xz; do
+    tar -C "$DIR" --force-local -xf "$1" "$COMPRESSION" && break
+  done
+}
+# substitute string of a file in rootfs
+SUB() {
+  FILE="${TMP}${1##*/}"
+  awk '{gsub("'"$2"'", "'"$3"'", $0);}1' "${DIR}$1" >|"${FILE}"
+  cp "${FILE}" "${DIR}$1"
+  # usage: SUB <path/to/file> <string> <new_string>
+}
+# filter url
+FILTER() {
+    awk -v REGEX="$1" '{match($0,REGEX,m); printf substr(m[0],2,length(m[0])-2)" "}' "repo_content" | awk '{printf $NF}'
+}
 # fetch & unpack packages
-for PKG in $PACKAGES
-do
-	REGEX=">$PKG-[0-9].*-$ARCH.*(gz|xz|zst)<"
-	FILTER="$(awk -v REGEX="$REGEX" '{match($0,REGEX,m); printf substr(m[0],2,length(m[0])-2)" "}' "repo_content" | awk '{printf $NF}')" &&
-	curl -LO --progress-bar "$URL/$FILTER" &&
-	TAR="tar -C $DIR --force-local -xf $PKG*"
-	eval "$TAR" --zstd ||
-	eval "$TAR" --gzip ||
-	eval "$TAR" --xz   || exit 1
+for PKG in $PACKAGES; do
+  REGEX=">$PKG-[0-9].*-$ARCH.*(gz|xz|zst)<"
+  PKGVER=$(FILTER "$REGEX")
+  printf '%s\n' "$PKGVER"
+  curl -LOC - --progress-bar "$URL/$PKGVER"
+  EXTRACT "$PKGVER" || exit 1
 done
 # DNS
-printf 'nameserver 9.9.9.9' > "$DIR/etc/resolv.conf"
-# Disabling signature checking for now
-cp "$DIR/etc/pacman.conf" .
-awk '{gsub ("^[[:space:]]*SigLevel.*$", "SigLevel = Never", $0); gsub ("^[[:space:]]*CheckSpace.*$", "#CheckSpace", $0);}1' "pacman.conf" > "$DIR/etc/pacman.conf" 
+printf 'nameserver 9.9.9.9' >"$DIR/etc/resolv.conf"
+# Replace Patterns
+for PATTERN in "/etc/pacman.conf ^.*CheckSpace #CheckSpace" "/etc/pacman.conf Required.*DatabaseOptional Never" "/etc/pacman.d/mirrorlist ^#Server Server"; do
+  SUB $PATTERN
+done
